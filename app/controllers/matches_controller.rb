b12213f4b71ead72258e5f9bd73ba0f820af48bd
daniel dantas <danieldantas0@hotmail.com>
2022-08-31 13:09:35 +0100

    match button

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,7 @@
 class MatchesController < ApplicationController
 
   def new
+    @match = Match.new
     males = User.where("gender ILIKE 'male' AND account_type ILIKE 'MatchSeeker'")
     @male = males.sample
     females = User.where("gender ILIKE 'female' AND account_type ILIKE 'MatchSeeker'")
@@ -8,5 +9,18 @@ class MatchesController < ApplicationController
   end
 
   def create
+    @match = Match.new(match_params)
+    @match.match_maker = current_user
+    if @match.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocesssable_entity
+    end
+  end
+
+  private
+
+  def match_params
+    params.require(:matches).permit(:male, :female)
   end
 end
