77c476d15bd0db71f0b3b3218c85121e14c16bff
daniel dantas <danieldantas0@hotmail.com>
2022-09-02 12:01:26 +0100

    logout + fonts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  delete '/users/sign_out' => 'sessions#destroy', :as => :destroy_user_session_2
+  
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
