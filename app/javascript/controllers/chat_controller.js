fca21500143969e7a85d160cd9840db4655aaeb7
sebastien verbist <sebastien.verbist@hotmail.com>
2022-09-06 11:41:41 +0100

    minor changes and close button start

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -10,9 +10,14 @@ export default class extends Controller {
 
   showChat(event) {
     let id = event.currentTarget.dataset.id;
-
     let target = this.matchTargets.find(match => match.dataset.id == id);
     this.matchTargets.forEach(card => card.classList.add("d-none"))
     target.classList.remove("d-none")
+  }
+
+  hideChat(event) {
+    let id = event.currentTarget.dataset.id;
+    let target = this.matchTargets.find(match => match.dataset.id == id);
+    this.matchTargets.forEach(card => card.classList.add("d-none"))
   }
 }
