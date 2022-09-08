5dac14678b0d69fe3b301922fb00b414f29d623b
LucaFauzia <luca.fauzia@live.com>
2022-09-08 16:39:12 +0100

    bug

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,7 +3,7 @@ class NotificationsController < ApplicationController
 
 
   def mark_all_read
-    @notification = current_user.notifications.mark_as_read!
+    @notifications = current_user.notifications.mark_as_read!
     redirect_to request.referrer
   end
 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :select]
+  skip_before_action :set_variables, only: [:home, :select]
+
   def home
   end
 
