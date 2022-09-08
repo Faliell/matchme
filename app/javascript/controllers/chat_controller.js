9ac6a823ce520955008bf5f3561052196dea0536
sebastien verbist <sebastien.verbist@hotmail.com>
2022-09-08 09:20:34 +0100

    Message card fixed

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -2,24 +2,37 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="chat"
 export default class extends Controller {
-  static targets = ["match"]
+  static targets = ["match", "chat"]
 
   connect() {
     console.log("CONNECTED")
-    console.log(this.hasMatchTarget)
+  }
+
+  showDescription(event) {
+    let id = event.currentTarget.dataset.id;
+    let target = this.matchTargets.find(match => match.dataset.id == id);
+    this.matchTargets.forEach(card => card.classList.add("d-none"))
+    this.chatTargets.forEach(card => card.classList.add("d-none"))
+    target.classList.remove("d-none")
+  }
+
+  hideDescription(event) {
+    this.matchTargets.forEach(card => card.classList.add("d-none"))
   }
 
   showChat(event) {
     let id = event.currentTarget.dataset.id;
-    console.log(id)
-    let target = this.matchTargets.find(match => match.dataset.id == id);
+    let target1 = this.chatTargets.find(chat => chat.dataset.id == id);
     this.matchTargets.forEach(card => card.classList.add("d-none"))
-    target.classList.remove("d-none")
+    this.chatTargets.forEach(card => card.classList.add("d-none"))
+    target1.classList.remove("d-none")
   }
 
   hideChat(event) {
-    let id = event.currentTarget.dataset.id;
-    let target = this.matchTargets.find(match => match.dataset.id == id);
-    this.matchTargets.forEach(card => card.classList.add("d-none"))
+    this.chatTargets.forEach(card => card.classList.add("d-none"))
   }
+
+
+
+
 }
